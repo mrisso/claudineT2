ef9d7b3760bcda3b93cc1a6f89f218c39e61b3d3
paiba <pedropaivaladeira@hotmail.com>
2017-07-15 12:32:45 -0300

    Funções de inserção, max, min e inicializa adicionadas

--- arvbb.c
+++ arvbb.c
@@ -10,6 +10,16 @@ struct arvbb{
 	Arvbb * dir;
 };
 
+Arvbb * Inicializa(palavra * chave){
+	Arvbb * no = malloc(sizeof(Arvbb));
+	no->pai=NULL;
+	no->esq=NULL;
+	no->dir=NULL;
+	no->palavra=chave;
+
+	return no;
+}
+
 void Percorre(Arvbb * raiz){
 	if(raiz != NULL){
 		Percorre(raiz->esq);
@@ -31,14 +41,31 @@ Arvbb * Pesquisa(Arvbb * raiz, char * chave){
 	return aux;
 }
 
-void Insere_arvbb(){
+Arvbb * Insere_arvbb(Arvbb * raiz, palavra * novo){
+	if(raiz==NULL)
+		return Inicializa(novo);
+	if(compareStrPalavra(getTexto(raiz->palavra),novo))
+		raiz->esq = Insere_arvbb(raiz->esq,novo);
+	else if (compareStrPalavra(getTexto(novo),raiz->palavra))
+		raiz->dir = Insere_arvbb(raiz->dir,novo);
+	return raiz;
 }
 
-Arvbb * Max_arvbb(){
-return NULL;
+Arvbb * Max_arvbb(Arvbb * raiz){//Função que encontra o maximo na arvore, indo sempre pra direita
+	Arvbb * aux = raiz;
+	while(raiz->dir != NULL)
+	{
+		aux = raiz->dir;
+	}
+	return aux;
 }
 
-Arvbb * Min_arvbb(){
-return NULL;
+Arvbb * Min_arvbb(Arvbb * raiz){//Função que encontra o minimo na arvore, indo sempre pra esquerda
+	Arvbb * aux = raiz;
+	while(raiz->esq != NULL)
+	{
+		aux = raiz->esq;
+	}
+	return aux;
 }
 
--- arvbb.h
+++ arvbb.h
@@ -3,14 +3,16 @@
 #include "palavra.h"
 typedef struct arvbb Arvbb;
 
+Arvbb * Inicializa(palavra * chave);
+
 void Percorre(Arvbb * raiz);
 
 Arvbb * Pesquisa(Arvbb * raiz, char * chave);
 
-void Insere_arvbb();
+Arvbb * Insere_arvbb();
 
-Arvbb * Max_arvbb();
+Arvbb * Max_arvbb(Arvbb * raiz);
 
-Arvbb * Max_arvbb();
+Arvbb * Max_arvbb(Arvbb * raiz);
 
 #endif // ARVBB_H_INCLUDED

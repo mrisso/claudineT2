306189a32a9d00f9cd7554a40a642bfcd6668fd9
paiba <pedropaivaladeira@hotmail.com>
2017-07-15 09:29:16 -0300

    Conserto de algumas coisas na estrutura

--- arvbb.c
+++ arvbb.c
@@ -9,16 +9,10 @@ struct arvbb{
 	Arvbb * dir;
 };
 
-Arvbb inicializa_arvbb(Palavra * palavra, Arvbb * noesquerdo, Arvbb * nodireito){
-	Arvbb * arvbb = malloc(sizeof Arvbb);
-
-	arvbb->palavra = malloc(sizeof Palavra);
+Arvbb * inicializa_arvbb(Palavra * palavra, Arvbb * noesquerdo, Arvbb * nodireito){
+	Arvbb * arvbb = malloc(sizeof(Arvbb));
 	arvbb->palavra = palavra;
-
-	arvbb->esq = malloc(sizeof Arvbb);
 	arvbb->esq = noesquerdo;
-
-	arvbb->dir = malloc(sizeof Arvbb);
 	arvbb->dir = nodireito;
 
 	return arvbb;
--- arvbb.h
+++ arvbb.h
@@ -3,6 +3,6 @@
 
 typedef struct arvbb Arvbb;
 
-Arvbb inicializa_arvbb();
+Arvbb * inicializa_arvbb(Palavra * palavra, Arvbb * noesquerdo, Arvbb * nodireito);
 
 #endif // ARVBB_H_INCLUDED

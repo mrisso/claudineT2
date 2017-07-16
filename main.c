1f6bc18cc42c22b078b23c742683baa6ca5fa6dd
Matheus H. Risso <mrisso@poli.ufrj.br>
2017-07-16 09:48:52 -0300

    Main de testes

--- main.c
+++ main.c
@@ -5,6 +5,32 @@
 
 int main(void)
 {
-    printf("01011100001110100101 Hello\n");
+	palavra *p = criaPalavra("aleatorio");
+	hash *hashMap = criarHash(4);
+
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("géépeto");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("gerundismo");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("claudia");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("simbionte");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("amigos");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("clientela");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("josias");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("claudinha");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("bochecho");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("dedinho");
+	addPalavraHash(hashMap, p);
+	p = criaPalavra("bochecho");
+	addPalavraHash(hashMap, p);
+
     return 0;
 }
